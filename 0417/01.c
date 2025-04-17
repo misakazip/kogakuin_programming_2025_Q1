4ffbb7133076b7cfdbafc3177017553e349e9148
Misaka <79515833+misakazip@users.noreply.github.com>
2025-04-17 04:52:46 +0000

    入力チェックを無理やり実装

--- 0417/01.c
+++ 0417/01.c
@@ -6,10 +6,15 @@ int main(void){
     scanf("%d", &tensu2);
     scanf("%d", &tensu3);
 
+    if((tensu1 > 100) || (tensu2 > 100) || (tensu3 > 100) || (tensu1 <0) || (tensu2 < 0) || (tensu3 < 0)){
+        printf("0以上100以下の数字を入力してください。");
+        return 1;
+    }
+
     heikin = (tensu1+tensu2+tensu3)/3;
 
     // 確認用
-    printf("3科目の平均得点は%dです。", heikin);
+    //printf("3科目の平均得点は%dです。", heikin);
 
     if (heikin >= 60){
         printf("合格");
